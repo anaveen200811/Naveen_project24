a523d3c1822ff430ba2e88e9db212234e5312f19
A Naveen <66691255+anaveen200811@users.noreply.github.com>
2020-07-04 10:53:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+
+
+var packageBody,ground1,ground,dustbin1,dustbin2,dustbin3,paper;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground1 = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground1);
+
+
+	 ground = new Ground(600,height,1200,20);
+	 dustbin1 = new Dustbin(600,height-40,200,20);
+	 dustbin2 = new Dustbin(500,height-100,20,120);
+	 dustbin3 = new Dustbin(700,height-100,20,120);
+	 paper = new Paper(200,450,70);
+ 
+	Engine.run(engine);
+	var render = Render.create({
+		element: document.body,
+		engine: engine,
+		options: {
+		  width: 1600,
+		  height: 700,
+		  wireframes: false
+		}
+	  });
+  
+	  Engine.run(engine);
+	  Render.run(render);
+	
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  ground.display();
+  dustbin1.display();
+  dustbin2.display();
+  dustbin3.display();
+  paper.display();
+  //keyPressed();
+  drawSprites();
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW ) {
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:65,y:-65});
+		
+	}
+	
+}
